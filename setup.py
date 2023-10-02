c21b088b90a311c0a611fe54cc4178aa19619f65
Ayhan Çağan <ayhancagan@Ayhans-MacBook-Pro.local>
2023-10-02 16:23:45 +0300

    requirements installation done

---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Text_Summarizer_Project"
+AUTHOR_USER_NAME = "ayhan-cagan"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "caganayhann@gmail.com"
+
+
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for NLP app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
+
+
+    
